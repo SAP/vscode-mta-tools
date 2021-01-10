69493c9a7dddf744c2925347ab4bed15447020a6
Frenkel <lior.frenkel@sap.com>
2021-01-10 11:15:37 +0200

    chore: resolving issues

--- src/validations/validationsFsWatcher.ts
+++ src/validations/validationsFsWatcher.ts
@@ -1,4 +1,5 @@
 import { mta, Mta } from "@sap/mta-lib";
+import { pathExists } from "fs-extra";
 import { keys, map } from "lodash";
 import { dirname, resolve } from "path";
 import {
@@ -39,67 +40,80 @@ export function watchMtaYamlAndDevExtFiles(disposables: Disposable[]): void {
     disposables
   );
 
-  async function addModuleDiagnostics(
-    uri: Uri,
-    disposables: Disposable[]
-  ): Promise<void> {
-    const modulePath = dirname(uri.fsPath);
+  workspace.onDidChangeWorkspaceFolders(
+    (e) => addModuleDiagnostics(Uri.file(e.added.toString()), disposables),
+    undefined,
+    disposables
+  );
+}
 
-    // Create the diagnostics collection. The collections are cached so that we can clear them in subsequent runs.
-    const moduleDiagnostics = getDiagnosticsCollection(
-      `Diagnostics for module: ${modulePath}`,
-      disposables
+async function addModuleDiagnostics(
+  uri: Uri,
+  disposables: Disposable[]
+): Promise<void> {
+  const modulePath = dirname(uri.fsPath);
+
+  // Create the diagnostics collection. The collections are cached so that we can clear them in subsequent runs.
+  const moduleDiagnostics = getDiagnosticsCollection(
+    `Diagnostics for module: ${modulePath}`,
+    disposables
+  );
+
+  const collectionEntries: [Uri, Diagnostic[] | undefined][] = [];
+  try {
+    // Clear existing entries in this collection from previous runs
+    // This is done "manually" and not via a call to clear() so that we only fire diagnostics changed event once
+    moduleDiagnostics.forEach((uri) =>
+      collectionEntries.push([uri, undefined])
     );
 
-    const collectionEntries: [Uri, Diagnostic[] | undefined][] = [];
-    try {
-      // Clear existing entries in this collection from previous runs
-      // This is done "manually" and not via a call to clear() so that we only fire diagnostics changed event once
-      moduleDiagnostics.forEach((uri) =>
-        collectionEntries.push([uri, undefined])
-      );
+    const diagnosticsByFile = await getMtaDiagnostics(modulePath);
 
-      const diagnosticsByFile = await getMtaDiagnostics(modulePath);
-
-      const newCollectionEntries = map(
-        diagnosticsByFile,
-        (fileDiagnostics, filePath): [Uri, Diagnostic[]] => [
-          Uri.file(filePath),
-          fileDiagnostics,
-        ]
-      );
-      collectionEntries.push(...newCollectionEntries);
-    } finally {
-      // Add the diagnostics to the collection. This updates the problems view.
-      moduleDiagnostics.set(collectionEntries);
-    }
+    const newCollectionEntries = map(
+      diagnosticsByFile,
+      (fileDiagnostics, filePath): [Uri, Diagnostic[]] => [
+        Uri.file(filePath),
+        fileDiagnostics,
+      ]
+    );
+    collectionEntries.push(...newCollectionEntries);
+  } finally {
+    // Add the diagnostics to the collection. This updates the problems view.
+    moduleDiagnostics.set(collectionEntries);
   }
+}
 
-  async function getMtaDiagnostics(
-    modulePath: string
-  ): Promise<Record<string, Diagnostic[]>> {
-    const diagnosticsByFile: Record<string, Diagnostic[]> = {};
-    const devMtaExtPath = resolve(modulePath, DEV_MTA_EXT);
+async function getMtaDiagnostics(
+  modulePath: string
+): Promise<Record<string, Diagnostic[]>> {
+  const diagnosticsByFile: Record<string, Diagnostic[]> = {};
+  const mtaPath = resolve(modulePath, MTA_YAML);
+  const devMtaExtPath = resolve(modulePath, DEV_MTA_EXT);
 
-    const mta = new Mta(modulePath, false, [devMtaExtPath]); // temp file is not relevant in our scenario
-
-    const validationRes: Record<string, mta.Issue[]> = await mta.validate();
-
-    for (const filePath of keys(validationRes)) {
-      diagnosticsByFile[filePath] = diagnosticsByFile[filePath] ?? [];
-
-      for (const issue of validationRes[filePath]) {
-        const position = new Position(issue.line, issue.column);
-        diagnosticsByFile[filePath].push({
-          message: issue.message,
-          range: new Range(position, position),
-          severity:
-            issue.severity === "error"
-              ? DiagnosticSeverity.Error
-              : DiagnosticSeverity.Warning,
-        });
-      }
-    }
+  if ((await pathExists(mtaPath)) === false) {
     return diagnosticsByFile;
   }
+
+  const devMtaExts: string[] | undefined =
+    (await pathExists(devMtaExtPath)) === true ? [devMtaExtPath] : undefined;
+  const mta = new Mta(modulePath, false, devMtaExts); // temp file is not relevant in our scenario
+
+  const validationRes: Record<string, mta.Issue[]> = await mta.validate();
+
+  for (const filePath of keys(validationRes)) {
+    diagnosticsByFile[filePath] = diagnosticsByFile[filePath] ?? [];
+
+    for (const issue of validationRes[filePath]) {
+      const position = new Position(issue.line, issue.column);
+      diagnosticsByFile[filePath].push({
+        message: issue.message,
+        range: new Range(position, position),
+        severity:
+          issue.severity === "error"
+            ? DiagnosticSeverity.Error
+            : DiagnosticSeverity.Warning,
+      });
+    }
+  }
+  return diagnosticsByFile;
 }
